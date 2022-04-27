3401d6c3f61f260e090a26b513317a0f6a7be498
tommyheaney <99109758+tommyheaney@users.noreply.github.com>
2022-04-27 21:26:35 +0100

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,53 @@
+console.log(2+2);
+            // "coercion"
+            document.write("10" + 5 +"<br>");
+
+            // "string"
+            document.write(typeof 'mukul'+"<br>");
+            
+            // "number"
+            document.write(typeof 25 +"<br>");
+            
+            // "undefined"
+            document.write(typeof variable +"<br>");
+
+            // "infinity"
+            document.write(2E310 +"<br>");
+
+            // "negative infinity"
+            document.write(-2E310 +"<br>");
+
+            // "Boolean"
+            document.write(10 > 2);
+
+            // "Boolean"
+            document.write(10 < 2);           
+
+            // "Double Equal Signs"
+            document.write(10 == 10);
+
+             // "Double Equal Signs"
+             document.write(5 > 3 && 10 > 4);
+
+             // "Double Equal Signs"
+             document.write(5 > 10 && 10 > 4);
+
+            // "Double Equal Signs"
+            document.write(5 > 10 || 10 > 4);
+
+            // "Double Equal Signs"
+            document.write(5>10 || 10 > 20); 
+
+// Script for the answering questions Button
+
+  function my_Click() {
+      document.getElementById("Not").innerHTML = !(5 > 10);
+  }
+
+  function my_Func() {
+    document.getElementById("Test").innerHTML = isNaN('String');
+    }
+
+    function my_tion() {
+      document.getElementById("NO").innerHTML = !(20 > 10);
+  }
